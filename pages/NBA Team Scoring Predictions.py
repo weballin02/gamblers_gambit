37cb99138bd0ef9a71900f8bc6ddcd0dc0337244
weballin02 <34046845+weballin02@users.noreply.github.com>
2024-11-21 18:18:21 -0800

    Update NBA Team Scoring Predictions.py
    remove login requirement

--- pages/NBA Team Scoring Predictions.py
+++ pages/NBA Team Scoring Predictions.py
@@ -22,11 +22,6 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
-# Restrict access if not logged in
-if "user" not in st.session_state:
-    st.error("Access denied. Please log in to view this page.")
-    st.stop()
-
 # General Styling and High Contrast Toggle
 st.markdown("""
     <style>
