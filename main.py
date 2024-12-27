bed33ade3cb5e15ed4d361c989a328edd1eb5f0b
weballin02 <34046845+weballin02@users.noreply.github.com>
2024-12-26 19:00:03 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ import os
 import firebase_admin
 from firebase_admin import credentials
 import datetime
+from streamlit_login_auth_ui.widgets import __login__
+
+__login__obj = __login__(auth_token = "pk_prod_2D86G0QECNM5HRJDRH06DATT8E3M", 
+                    company_name = "FoxEdge",
+                    width = 200, height = 250, 
+                    logout_button_name = 'Logout', hide_menu_bool = False, 
+                    hide_footer_bool = False, 
+                    lottie_url = 'https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json')
+
+LOGGED_IN = __login__obj.build_login_ui()
+
+if LOGGED_IN == True:
+
+    st.markown("Your Streamlit Application Begins here!")
 
 # Define icons directory path
 ICONS_DIR = "icons"  # Ensure this is set correctly
